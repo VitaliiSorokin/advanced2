bdad10a89d7cbfbdf9252352db9355d3cf734c06
VitaliiSorokin <x3meast@gmail.com>
2017-12-08 21:20:44 +0200

    first commit

---
+++ src/com/level/Level.java
@@ -0,0 +1,23 @@
+package com.level;
+
+import java.util.Scanner;
+
+public class Level {
+    public static void main(String[] args) {
+        System.out.println("Type a number: ");
+        System.out.println(new Scanner(System.in).nextInt() % 2 == 0 ? "Even" : "Odd");
+    }
+
+    public int[] swapMinMax(int[] arr) {
+
+            for (int i = 0; i < arr.length; i++) {
+                System.out.println(arr[i]);
+                if (arr[i] == 0) {
+                    break;
+                }
+            }
+
+
+        return  null;
+    }
+}
